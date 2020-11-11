cb4e88f52b95089abca0f65acbe03c63a5cf98b5
gajan@jp.ibm.com <gajan@jp.ibm.com>
2020-11-11 01:51:07 +0000

    script added

---
+++ scripts/make_bundle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+make bundle
+
+make bundle-build BUNDLE_IMG=quay.io/gajananan/integrity-enforcer-operator-bundle:0.0.22dev
+
+
+docker push quay.io/gajananan/integrity-enforcer-operator-bundle:0.0.22dev
+
+opm index add -c docker --generate --bundles quay.io/gajananan/integrity-enforcer-operator-bundle:0.0.22dev \
+                      --from-index quay.io/gajananan/integrity-enforcer-operator-index:0.0.21dev \
+                      --tag quay.io/gajananan/integrity-enforcer-operator-index:0.0.22dev --out-dockerfile tmp.Dockerfile
+                      
+rm tmp.Dockerfile
+
+docker build -f index.Dockerfile -t quay.io/gajananan/integrity-enforcer-operator-index:0.0.22dev . --no-cache
+
+docker push quay.io/gajananan/integrity-enforcer-operator-index:0.0.22dev
