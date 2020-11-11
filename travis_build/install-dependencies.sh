8a2feba4e9736bbd3d31c1925329a68e8b3991b9
gajan@jp.ibm.com <gajan@jp.ibm.com>
2020-11-11 05:02:50 +0000

    fixed

--- travis_build/install-dependencies.sh
+++ travis_build/install-dependencies.sh
@@ -7,7 +7,6 @@ OPERATOR_SDK_VERSION=v1.1.0
 curl -LO https://github.com/operator-framework/operator-sdk/releases/download/$OPERATOR_SDK_VERSION/operator-sdk-$OPERATOR_SDK_VERSION-x86_64-linux-gnu
 chmod +x operator-sdk-$OPERATOR_SDK_VERSION-x86_64-linux-gnu && sudo mkdir -p /usr/local/bin/ && sudo cp operator-sdk-$OPERATOR_SDK_VERSION-x86_64-linux-gnu /usr/local/bin/operator-sdk && rm operator-sdk-$OPERATOR_SDK_VERSION-x86_64-linux-gnu
 
-
 operator-sdk version
 
 OPM_VERSION=v1.15.1
@@ -24,3 +23,5 @@ wget -nv $OPM_URL -O $GOPATH/bin/opm
 chmod +x $GOPATH/bin/opm
 
 $GOPATH/bin/opm version
+
+echo "Finished setting up."
