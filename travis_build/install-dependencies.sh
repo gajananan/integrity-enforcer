c5cd7360c66851ff1b7214e844f8799e71434d55
Kugamoorthy Gajananan <gajananan@gmail.com>
2020-11-11 16:03:23 +0900

    updated

--- travis_build/install-dependencies.sh
+++ travis_build/install-dependencies.sh
@@ -25,7 +25,8 @@ $GOPATH/bin/opm version
 
 
 if [[ "$OS_NAME" == "Linux" ]]; then
-    curl -s "https://raw.githubusercontent.com/\\nkubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+    curl -s "https://raw.githubusercontent.com/\
+             kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 fi
 
 chmod +x ./kustomize
