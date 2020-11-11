7b5485d228f367622bdca8bd16d3149e96d26542
gajan@jp.ibm.com <gajan@jp.ibm.com>
2020-11-11 06:19:58 +0000

    fixed

--- scripts/make_bundle.sh
+++ scripts/make_bundle.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $IE_REPO_ROOT/integrity-enforcer-operator
+
 make bundle
 
 make bundle-build BUNDLE_IMG=quay.io/gajananan/integrity-enforcer-operator-bundle:0.0.22dev
@@ -10,7 +12,7 @@ docker push quay.io/gajananan/integrity-enforcer-operator-bundle:0.0.22dev
 opm index add -c docker --generate --bundles quay.io/gajananan/integrity-enforcer-operator-bundle:0.0.22dev \
                       --from-index quay.io/gajananan/integrity-enforcer-operator-index:0.0.21dev \
                       --tag quay.io/gajananan/integrity-enforcer-operator-index:0.0.22dev --out-dockerfile tmp.Dockerfile
-                      
+
 rm tmp.Dockerfile
 
 docker build -f index.Dockerfile -t quay.io/gajananan/integrity-enforcer-operator-index:0.0.22dev . --no-cache
