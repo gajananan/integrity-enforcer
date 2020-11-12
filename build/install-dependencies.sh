ac3ed8e32e21b5f26760c24564ccac643c4802a0
gajan@jp.ibm.com <gajan@jp.ibm.com>
2020-11-12 01:30:49 +0000

    fixed

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -12,7 +12,7 @@ if ! [ -x "$(command -v operator-sdk)" ]; then
 
 	if [[ "$OS_NAME" == "Linux" ]]; then
 		curl -LO https://github.com/operator-framework/operator-sdk/releases/download/$OPERATOR_SDK_VERSION/operator-sdk-$OPERATOR_SDK_VERSION-x86_64-linux-gnu -o operator-sdk
-	elif
+	elif [[ "$OS_NAME" == "Darwin" ]]; then
 		curl -LO https://github.com/operator-framework/operator-sdk/releases/download/$OPERATOR_SDK_VERSION/operator-sdk-$OPERATOR_SDK_VERSION-x86_64-apple-darwin -o operator-sdk
 	fi
 	chmod +x operator-sdk
